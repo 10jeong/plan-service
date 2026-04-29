f0e1ddfe7ffafba609c3acb88828b3826593f700
oni128 <lyw283828@gmail.com>
2026-04-29 19:32:58 +0900

    chore: 불필요한 코드 제거

--- src/main/java/com/yeoljeong/tripmate/PlanServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/PlanServiceApplication.java
@@ -2,14 +2,10 @@ package com.yeoljeong.tripmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan("com.yeoljeong.tripmate.domain.model")
-@EnableJpaRepositories("com.yeoljeong.tripmate")
 public class PlanServiceApplication {
 
   public static void main(String[] args) {
