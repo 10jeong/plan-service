9e1dbecead47b213890e910eff462cd217ad1f1c
oni128 <lyw283828@gmail.com>
2026-05-03 05:07:46 +0900

    refactor: @Transactional(readOnly = true) 추가

--- src/main/java/com/yeoljeong/tripmate/application/service/query/PlanQueryService.java
+++ src/main/java/com/yeoljeong/tripmate/application/service/query/PlanQueryService.java
@@ -16,9 +16,11 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PlanQueryService {
 
   private final PlanRepository planRepository;
