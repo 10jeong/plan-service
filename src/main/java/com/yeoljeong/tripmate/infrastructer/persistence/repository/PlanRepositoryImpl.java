f7e9d2ce5a25a3b2495f031bc3c2f4d26f9f9a21
oni128 <lyw283828@gmail.com>
2026-05-03 06:55:29 +0900

    feat: 일정 목록 조회 로직 구현

--- src/main/java/com/yeoljeong/tripmate/infrastructer/persistence/repository/PlanRepositoryImpl.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructer/persistence/repository/PlanRepositoryImpl.java
@@ -1,11 +1,15 @@
 package com.yeoljeong.tripmate.infrastructer.persistence.repository;
 
+import com.yeoljeong.tripmate.domain.enums.RecruitStatus;
 import com.yeoljeong.tripmate.domain.model.plan.Plan;
 import com.yeoljeong.tripmate.domain.repository.PlanRepository;
 import com.yeoljeong.tripmate.infrastructer.persistence.jpa.PlanJpaRepository;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -27,5 +31,11 @@ public class PlanRepositoryImpl implements PlanRepository {
   @Override
   public Optional<Plan> findById(UUID planId) {
     return jpaRepository.findById(planId);
+  }
+
+  @Override
+  public Slice<Plan> findByCondition(String title, LocalDate startDate, LocalDate endDate,
+      RecruitStatus recruitStatus, Pageable pageable) {
+    return jpaRepository.findByCondition(title, startDate, endDate, recruitStatus, pageable);
   }
 }
