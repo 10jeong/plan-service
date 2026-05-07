b69e9344a67bc4f41313adbfe9c0efb5ea8379d9
oni128 <lyw283828@gmail.com>
2026-05-07 20:25:21 +0900

    feat: 결재 이벤트 수신 로직 추가

--- src/main/java/com/yeoljeong/tripmate/application/service/command/PlanInternalCommandService.java
+++ src/main/java/com/yeoljeong/tripmate/application/service/command/PlanInternalCommandService.java
@@ -2,17 +2,18 @@ package com.yeoljeong.tripmate.application.service.command;
 
 import com.yeoljeong.tripmate.application.dto.result.FindParticipationStatusResult;
 import com.yeoljeong.tripmate.application.port.PlanEvents;
+import com.yeoljeong.tripmate.domain.enums.ParticipationStatus;
 import com.yeoljeong.tripmate.domain.exception.PlanErrorCode;
 import com.yeoljeong.tripmate.domain.model.PlanParticipation;
 import com.yeoljeong.tripmate.domain.model.PlanUnit;
 import com.yeoljeong.tripmate.domain.repository.PlanParticipationRepository;
 import com.yeoljeong.tripmate.domain.repository.PlanUnitRepository;
 import com.yeoljeong.tripmate.event.OrderCreatedEvent;
+import com.yeoljeong.tripmate.event.PaymentCompletedEvent;
 import com.yeoljeong.tripmate.event.PlanUnitParticipantAddedEvent;
 import com.yeoljeong.tripmate.exception.BusinessException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,6 +35,9 @@ public class PlanInternalCommandService {
         participation.getParticipationStatus());
   }
 
+  /*
+  * 주문 생성시, 참여인원 증가 및 상태 변경(APPROVED -> RESERVED)
+  * */
   public void addPlanUnitParticipant(OrderCreatedEvent event) {
 
     // todo: 멱등성 처리
@@ -64,4 +68,26 @@ public class PlanInternalCommandService {
     events.planUnitAddParticipant(new PlanUnitParticipantAddedEvent(event.eventId(), event.productId(), event.scheduleId(), event.quantity()));
 
   }
+
+  /*
+  * 결제 완료시, 참여 상태 변경 (RESERVED -> CONFIRMED)
+  * */
+  public void updateParticipantStatus(PaymentCompletedEvent event) {
+    // todo: event에 planUnitId가 들어올때 수정
+    UUID planUnitId = UUID.randomUUID();
+    PlanParticipation participation = planParticipationRepository.findByPlanUnit_IdAndUserId(planUnitId, event.userId())
+        .orElseThrow(() -> new BusinessException(PlanErrorCode.PLAN_PARTICIPATION_NOT_FOUND));
+    participation.validatePlanParticipationStatus(participation.getParticipationStatus());
+
+    // 상태 변경 (RESERVED -> CONFIRMED)
+    int updated = planParticipationRepository.updateStatus(
+        participation.getPlanUnit().getId(),
+        participation.getParticipationStatus(),
+        ParticipationStatus.CONFIRMED);
+
+    if (updated == 0) {
+      throw new BusinessException(PlanErrorCode.PLAN_PARTICIPATION_STATUS_CHANGE_INVALID);
+    }
+  }
+
 }
--- src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/consumer/OrderCreatedEventListener.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/consumer/OrderCreatedEventListener.java
@@ -2,6 +2,7 @@ package com.yeoljeong.tripmate.infrastructer.messaging.consumer;
 
 import com.yeoljeong.tripmate.application.service.command.PlanInternalCommandService;
 import com.yeoljeong.tripmate.event.OrderCreatedEvent;
+import com.yeoljeong.tripmate.event.PaymentCompletedEvent;
 import com.yeoljeong.tripmate.event.enums.OrderTopic;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +29,21 @@ public class OrderCreatedEventListener {
 
       planInternalCommandService.addPlanUnitParticipant(event);
       log.info("[plan-service] 메시지 업데이트 처리 완료 : eventId={} ", event.eventId());
+
+    } catch (Exception e) {
+      log.error("[plan-service] 메시지 업데이트 처리 실패 : {} ", e.getMessage(), e);
+      throw e;
+    }
+  }
+
+  public void paymentCompleted(PaymentCompletedEvent event) {
+    try {
+      if (event == null) {
+        log.warn("[plan-service] 결제 완료 이벤트가 null입니다.");
+        return;
+      }
+      planInternalCommandService.updateParticipantStatus(event);
+      log.info("[plan-service] 메시지 업데이트 처리 완료 : eventId={} ", event.eventHash());
 
     } catch (Exception e) {
       log.error("[plan-service] 메시지 업데이트 처리 실패 : {} ", e.getMessage(), e);
