095b97d7aef453e8ed4a3484a16b765792ff5608
oni128 <lyw283828@gmail.com>
2026-05-09 00:02:20 +0900

    refactor: 수정된 이벤트 객체로 반영

--- src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/consumer/OrderEventListener.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/consumer/OrderEventListener.java
@@ -3,6 +3,7 @@ package com.yeoljeong.tripmate.infrastructer.messaging.consumer;
 import com.yeoljeong.tripmate.application.service.command.PlanInternalCommandService;
 import com.yeoljeong.tripmate.event.OrderCreatedEvent;
 import com.yeoljeong.tripmate.event.enums.OrderTopic;
+import com.yeoljeong.tripmate.infrastructer.messaging.KafkaPayloadDeserializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,12 +15,15 @@ import org.springframework.stereotype.Component;
 public class OrderEventListener {
 
   private final PlanInternalCommandService planInternalCommandService;
+  private final KafkaPayloadDeserializer kafkaPayloadDeserializer;
 
   @KafkaListener (topics = OrderTopic.ORDER_CREATED_TOPIC, groupId = "plan-group")
-  public void orderCreated(OrderCreatedEvent event) { // todo: ack 고려
+  public void orderCreated(String s) { // todo: ack 고려
     try {
 
       // todo: 이미 처리된 이벤트인지 확인(멱등성 추후 처리)
+
+      OrderCreatedEvent event = kafkaPayloadDeserializer.deserialize(s, OrderCreatedEvent.class);
 
       if (event == null) {
         log.warn("[plan-service] 주문 생성 이벤트가 null입니다.");
