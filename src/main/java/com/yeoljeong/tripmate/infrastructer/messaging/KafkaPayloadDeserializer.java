9251b5b789b32893e74d06914a53f8d16fe7d481
oni128 <lyw283828@gmail.com>
2026-05-10 16:38:12 +0900

    feat: kafka DLT 설정 추가

--- src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/KafkaPayloadDeserializer.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructer/messaging/KafkaPayloadDeserializer.java
@@ -1,5 +1,6 @@
 package com.yeoljeong.tripmate.infrastructer.messaging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +12,12 @@ import org.springframework.stereotype.Component;
 public class KafkaPayloadDeserializer {
   private final ObjectMapper objectMapper;
 
-  public <T> T deserialize(String payload, Class<T> clazz) {
+  public <T> T deserialize(String payload, Class<T> clazz) throws JsonProcessingException {
     try {
       return objectMapper.readValue(payload, clazz);
     } catch (Exception e) {
       log.error("[KAFKA] 역직렬화 실패 - class {},", clazz, e);
-      throw new RuntimeException(e);
+      throw e;
     }
   }
 }
